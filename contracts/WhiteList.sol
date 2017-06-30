b9d17af3319e65706768862965169f34930a27ab
human <dip@dip-office.net>
2017-06-30 18:57:05 +0200

    [+] add admin restriction for more safety

--- contracts/WhiteList.sol
+++ contracts/WhiteList.sol
@@ -6,11 +6,13 @@ contract WhiteList {
     uint16  public chunkNr = 0;
     uint256 public controlSum = 0;
     bool public isSetupMode = true;
+    address admin = msg.sender;
 
     //adds next address package to the internal white list.
     //call valid only in setup mode.
     function addPack(address[] addrs, uint16 _chunkNr)
     setupOnly
+    adminOnly
     external {
         require ( chunkNr++ == _chunkNr);
         for(uint16 i=0; i<addrs.length; ++i){
@@ -20,12 +22,19 @@ contract WhiteList {
     }
 
     //disable setup mode
-    function start() public {
+    function start()
+    adminOnly
+    public {
         isSetupMode = false;
     }
 
     modifier setupOnly {
         if (!isSetupMode) throw;
+        _;
+    }
+
+    modifier adminOnly {
+        if (msg.sender != admin) throw;
         _;
     }
 }
